0b6dce362eb378e91f450efac07a740514d03fae
mingki <mg_1126@naver.com>
2022-12-13 20:47:35 +0900

    2022.12.13

--- SQL/SET.sql
+++ SQL/SET.sql
@@ -10,6 +10,7 @@ FROM departments;
 --중복된 값이 없음
 --자동으로 정렬
 
+
 --UNION ALL
 --예제 6-5
 SELECT department_id
@@ -19,3 +20,22 @@ SELECT department_id
 FROM departments
 ORDER BY department_id;
 --정렬되지 않음
+
+
+--INTERSECT
+--예제 6-6
+SELECT department_id
+FROM employees
+INTERSECT
+SELECT department_id
+FROM departments
+ORDER BY department_id;
+
+
+--MINUS
+--예제 6-7
+SELECT department_id
+FROM departments
+MINUS
+SELECT department_id
+FROM employees;
