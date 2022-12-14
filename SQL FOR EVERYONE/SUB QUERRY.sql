24eb4fefc92310c4a0048d4fb2b1a9f87ada29ff
mingki <mg_1126@naver.com>
2022-12-14 20:32:30 +0900

    2022.12.14

--- SQL FOR EVERYONE/SUB QUERRY.sql
+++ SQL FOR EVERYONE/SUB QUERRY.sql
@@ -3,6 +3,7 @@
 
 --단일 행 서브쿼리
 --예제 7-1
+
 SELECT *
 FROM employees A
 
@@ -11,9 +12,11 @@ WHERE A.salary = (
                   FROM employees
                   WHERE last_name = 'De Haan'
                   );
-                  
+          
+          
 --다중 행 서브쿼리
 --예제 7-2
+
 SELECT *
 FROM employees A
 
@@ -22,4 +25,30 @@ WHERE A.salary IN (
                    FROM employees
                    GROUP BY department_id
                    )
-ORDER BY A.salary DESC;                   
+ORDER BY A.salary DESC;
+
+
+--다중 열 서브쿼리
+--예제 7-3
+
+SELECT *
+FROM employees A
+WHERE (A.job_id, A.salary) IN (
+                                SELECT job_id, MIN(salary) 그룹별급여
+                                FROM employees
+                                GROUP BY job_id
+                                )
+ORDER BY A.salary DESC;
+
+
+--FROM 절 서브쿼리
+--예제 7-4
+
+SELECT *
+FROM employees A,
+                ( 
+                SELECT department_id
+                FROM departments
+                WHERE department_name = 'IT'
+                ) B
+WHERE A.department_id = B.department_id;           
