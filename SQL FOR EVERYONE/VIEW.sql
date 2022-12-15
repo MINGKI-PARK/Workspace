a7af76886394f99fe2ecbf820ad042853e007be2
mingki <mg_1126@naver.com>
2022-12-15 21:29:40 +0900

    2022.12.15

--- SQL FOR EVERYONE/VIEW.sql
+++ SQL FOR EVERYONE/VIEW.sql
@@ -1,1 +1,8 @@
---관계형 데이터베이스의 주요 지식
+--뷰: 가상의 테이블
+
+--예제 11-1
+
+SELECT A.employee_id, A.hire_date, B.department_name, B.job_title
+FROM employees A, emp_details_view B
+WHERE A.employee_id = B.employee_id
+AND A.employee_id = 100;
