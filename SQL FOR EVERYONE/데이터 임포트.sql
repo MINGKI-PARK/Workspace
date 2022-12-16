99383efd1786e6115ad0e9754f85aa32fc555ab6
mingki <mg_1126@naver.com>
2022-12-16 23:03:20 +0900

    2022.12.16

---
+++ SQL FOR EVERYONE/데이터 임포트.sql
@@ -0,0 +1,14 @@
+--테이블 생성
+--@C:\모두의SQL\create_table.sql;
+
+
+--데이터 삽입
+--@C:\모두의SQL\1.address.sql;
+--@C:\모두의SQL\2.customer.sql;
+--@C:\모두의SQL\3.item.sql;
+--@C:\모두의SQL\4.reservation.sql;
+--@C:\모두의SQL\5.order_info.sql;
+
+
+SELECT *
+FROM order_info;
