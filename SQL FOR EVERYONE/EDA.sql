083d6684f58ae110eff36ce0bf9a39420034661f
mingki <mg_1126@naver.com>
2022-12-27 23:01:36 +0900

    2022.12.27

--- SQL FOR EVERYONE/EDA.sql
+++ SQL FOR EVERYONE/EDA.sql
@@ -89,10 +89,11 @@ SELECT SUBSTR(A.reserv_date, 1, 6) 매출월,
        SUM(decode(B.item_id, 'M0001', B.sales,0)) 전용상품매출,
        ROUND(SUM(DECODE(B.item_id, 'M0001', B.sales, 0))/SUM(B.sales)*100, 1) 매출기여율
        
-FROM rservation A, order_info B
+FROM reservation A, order_info B
 WHERE A.reserv_no = B.reserv_no
 AND A.cancel = 'N'
-GROUP BY
+GROUP BY SUBSTR(A.reserv_date, 1, 6)
+ORDER BY SUBSTR(A.reserv_date, 1, 6);
 
 
 
