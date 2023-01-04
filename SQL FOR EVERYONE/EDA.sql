a37d1010cfcbbd65d847b50114cf9404ea36ecfe
mingki <mg_1126@naver.com>
2023-01-05 01:22:40 +0900

    2023.01.05

--- SQL FOR EVERYONE/EDA.sql
+++ SQL FOR EVERYONE/EDA.sql
@@ -150,3 +150,30 @@ GROUP BY SUBSTR(A.reserv_date, 1, 6)
 ORDER BY SUBSTR(A.reserv_date, 1, 6);
 
 
+--요일별 매출 분석: 날짜 처리하기
+--분석9 월별 전용 상품 매출액을 일요일부터 월요일까지 구분해 출력
+
+SELECT SUBSTR(reserv_date, 1, 6) 날짜,
+       A.product_name 상품명,
+       SUM(DECODE(A.WEEK, '1', A.sales, 0)) 일요일,
+       SUM(DECODE(A.WEEK, '2', A.sales, 0)) 월요일,
+       SUM(DECODE(A.WEEK, '3', A.sales, 0)) 화요일,
+       SUM(DECODE(A.WEEK, '4', A.sales, 0)) 수요일,
+       SUM(DECODE(A.WEEK, '5', A.sales, 0)) 목요일,
+       SUM(DECODE(A.WEEK, '6', A.sales, 0)) 금요일,
+       SUM(DECODE(A.WEEK, '7', A.sales, 0)) 토요일
+       
+FROM
+    (
+    SELECT A.reserv_date,
+           C.product_name,
+           TO_CHAR(TO_DATE(A.reserv_date, 'YYYYMMDD') , 'd') WEEK,
+           B.sales
+    FROM reservation A, order_info B, item C
+    WHERE A.reserv_no = B.reserv_no
+    AND B.item_id = C.item_id
+    AND B.item_id = 'M0001'
+    ) A
+GROUP BY SUBSTR(reserv_date, 1, 6), A.product_name
+ORDER BY SUBSTR(reserv_date, 1, 6);
+
