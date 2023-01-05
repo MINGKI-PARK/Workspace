6faaa997c22b27ae07a8411623dabc9f99ca0282
mingki <mg_1126@naver.com>
2023-01-05 23:29:50 +0900

    2023.01.05

--- SQL FOR EVERYONE/EDA.sql
+++ SQL FOR EVERYONE/EDA.sql
@@ -184,9 +184,9 @@ ORDER BY SUBSTR(reserv_date, 1, 6);
 SELECT  * 
 FROM (
         SELECT SUBSTR(A.reserv_date, 1, 6) 매출월,
-        A.branch 지점,
-        SUM(B.sales) 전용상품매출,
-        RANK() OVER(PARTITION BY SUBSTR(A.reserv_date, 1, 6) ORDER BY SUM(B.sales) DESC) 지점순위,
+               A.branch 지점,
+               SUM(B.sales) 전용상품매출,
+               RANK() OVER(PARTITION BY SUBSTR(A.reserv_date, 1, 6) ORDER BY SUM(B.sales) DESC) 지점순위
         FROM reservation A, order_info B
         WHERE A.reserv_no = B.reserv_no
         AND A.cancel = 'N'
@@ -194,4 +194,24 @@ FROM (
         GROUP BY SUBSTR(A.reserv_date, 1, 6), A.branch
         ORDER BY SUBSTR(A.reserv_date, 1, 6)
      ) A
-WHERE A.지점순위 <= 3;     
+WHERE A.지점순위 <= 3;
+
+
+--1위 지점만 다시 조회
+
+SELECT *
+FROM (
+        SELECT SUBSTR(A.reserv_date, 1, 6) 매출월,
+               A.branch 지점,
+               SUM(B.sales) 전용상품매출,
+               ROW_NUMBER() OVER(PARTITION BY SUBSTR(A.reserv_date, 1, 6) ORDER BY SUM(B.sales) DESC) 지점순위,
+               DECODE(A.branch, '강남', 'A', '종로', 'A', '영등포', 'A', 'B') 지점등급
+        FROM reservation A, order_info B
+        WHERE A.reserv_no = B.reserv_no
+        AND A.cancel = 'N'
+        AND B.item_id = 'M0001'
+        GROUP BY SUBSTR(A.reserv_date, 1, 6), A.branch, DECODE(A.branch, '강남', 'A', '종로', 'A', '영등포', 'A', 'B')
+        ORDER BY SUBSTR(A.reserv_date, 1, 6)
+     ) A
+WHERE A.지점순위 = 1;
+--AND 지점등급 = 'A';
