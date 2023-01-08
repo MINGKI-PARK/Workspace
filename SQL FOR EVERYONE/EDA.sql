f49093fa48fc706baa1e7f5fa9d3a8451de05088
mingki <mg_1126@naver.com>
2023-01-09 02:13:31 +0900

    2023.01.09

--- SQL FOR EVERYONE/EDA.sql
+++ SQL FOR EVERYONE/EDA.sql
@@ -136,12 +136,12 @@ SELECT SUBSTR(A.reserv_date, 1, 6) 매출월,
        SUM(B.sales) 총매출,
        SUM(B.sales) - SUM(DECODE(B.item_id, 'M0001', B.sales, 0)) 전용상품외매출,
        SUM(DECODE(B.item_id, 'M0001', B.sales, 0)) 전용상품매출,
-       ROUND(SUM(DECODE(B.item_id, 'M0001', B.sales, 0)) / SUM(B.sales) * 100, 1)||'%' 전용상품판매율,
+       ROUND(SUM(DECODE(B.item_id, 'M0001', B.sales, 0)) / SUM(B.sales) * 100, 1) || '%' 전용상품판매율,
        
        COUNT(A.reserv_no) 총예약건,
        SUM(DECODE(A.cancel, 'N', 1, 0)) 예약완료건,
        SUM(DECODE(A.cancel, 'Y', 1, 0)) 예약취소건,
-       ROUND(SUM(DECODE(A.cancel, 'Y', 1, 0)) / COUNT(A.reserv_no) * 100, 1)||'%' 예약취소율
+       ROUND(SUM(DECODE(A.cancel, 'Y', 1, 0)) / COUNT(A.reserv_no) * 100, 1) || '%' 예약취소율
        
 FROM reservation A, order_info B
 WHERE A.reserv_no = B.reserv_no(+)
@@ -231,20 +231,23 @@ SELECT A.매출월 매출월,
        MAX(예약취소율) 예약취소율,
        MAX(최대매출지점) 최대매출지점,
        MAX(지점매출액) 지점매출액
+
 FROM (
         SELECT SUBSTR(A.reserv_date, 1, 6) 매출월,
                SUM(B.sales) 총매출,
                SUM(B.sales) - SUM(DECODE(B.item_id, 'M0001', B.sales, 0)) 전용상품외매출,
                SUM(DECODE(B.item_id, 'M0001', B.sales, 0)) 전용상품매출,
-               ROUND(SUM(DECODE(B.item_id, 'M0001', B.sales, 0))/SUM(B.sales)*100, 1)||'%' 전용상품판매율,
+               ROUND(SUM(DECODE(B.item_id, 'M0001', B.sales, 0)) / SUM(B.sales) * 100, 1) || '%' 전용상품판매율,
                COUNT(A.reserv_no) 총예약건,
                SUM(DECODE(A.cancel, 'N', 1, 0)) 예약완료건,
                SUM(DECODE(A.cacnel, 'Y', 1, 0)) 예약취소건,
-               ROUND(SUM(DECODE(A.cacnel, 'Y', 1, 0))/COUNT(A.reserv_no)*100, 1)||'%' 예약취소율,
+               ROUND(SUM(DECODE(A.cacnel, 'Y', 1, 0)) / COUNT(A.reserv_no) * 100, 1) || '%' 예약취소율,
                '' 최대매출지점,
-               0 지점매출액
+               0  지점매출액
+               
          FROM reservation A, order_info B
          WHERE A.reserv_no = B.reserv_no(+)
+--         AND A.cancel = 'N'
          GROUP BY SUBSTR(A.reserv_date, 1, 6), '', 0
 
     UNION
@@ -277,3 +280,23 @@ FROM (
 GROUP BY A.매출월
 ORDER BY A.매출월;
                    
+
+
+
+
+SELECT SUBSTR(A.reserv_date,1,6) 매출월, 
+           SUM(B.sales) 총매출, 
+           SUM(B.sales)
+           - SUM(DECODE(B.item_id,'M0001',B.sales,0)) 전용상품외매출, 
+           SUM(DECODE(B.item_id,'M0001',B.sales,0)) 전용상품매출,
+           ROUND(SUM(DECODE(B.item_id,'M0001',B.sales,0))/SUM(B.sales)*100,1)||'%' 전용상품판매율,
+           COUNT(A.reserv_no) 총예약건,
+           SUM(DECODE(A.cancel,'N',1,0)) 예약완료건,
+           SUM(DECODE(A.cancel,'Y',1,0)) 예약취소건,
+           ROUND(SUM(DECODE(A.cancel,'Y',1,0))/COUNT(A.reserv_no)*100,1)||'%' 예약취소율,
+           '' 최대매출지점,
+           0  지점매출액
+    FROM reservation A, order_info B
+    WHERE A.reserv_no = B.reserv_no(+)
+    -- AND   A.cancel    = 'N'
+    GROUP BY SUBSTR(A.reserv_date,1,6), '', 0
