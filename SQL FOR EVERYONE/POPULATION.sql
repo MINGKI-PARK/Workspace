a40ce6abeba1a47d6d06e3067834f4b7c409bc81
mingki <mg_1126@naver.com>
2023-01-11 22:45:43 +0900

    2023.01.11

--- SQL FOR EVERYONE/POPULATION.sql
+++ SQL FOR EVERYONE/POPULATION.sql
@@ -6,4 +6,8 @@ SELECT COUNT(customer_id) 고객수,
        SUM(DECODE(sex_code, 'F', 1, 0)) 여자,
        ROUND(AVG(MONTHS_BETWEEN(TO_DATE('20171231', 'YYYYMMDD'), TO_DATE(birth, 'YYYYMMDD')) / 12), 1) 평균나이,
        ROUND(AVG(MONTHS_BETWEEN(TO_DATE('20171231', 'YYYYMMDD'), first_reg_date)), 1) 평균거래기간
-FROM customer;       
+FROM customer;
+
+
+SELECT *
+FROM customer;
