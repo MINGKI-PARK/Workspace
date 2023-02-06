d034f9a7a95dbe57c28b2e791b2a1fa0730c4572
mingki <mg_1126@naver.com>
2023-02-06 18:50:33 +0900

    2023.02.6

--- SQL/PROGRAMMERS/LV3_있었는데요 없었습니다.sql
+++ SQL/PROGRAMMERS/LV3_있었는데요 없었습니다.sql
@@ -5,5 +5,7 @@ SELECT A.animal_id, A.name
 FROM animal_ins A INNER JOIN animal_outs B
 ON A.animal_id = B.animal_id
 # WHERE DATEDIFF(B.datetime, A.datetime) < 0
+# TIMEDIFF를 쓴다면 해결할 수 있음
+# DATEDIFF는 날짜만 계산. 날짜가 같다면 시간이 빨라도 0으로 나옴.
 WHERE A.datetime > B.datetime
 ORDER BY A.datetime
